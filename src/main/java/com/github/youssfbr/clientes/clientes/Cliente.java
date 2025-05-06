d7102812c76329472669dcea2ba47a9ee498200c
Alisson Youssf <youssfbr@gmail.com>
2025-05-06 19:08:12 -0300

    Initial commit

---
+++ src/main/java/com/github/youssfbr/clientes/clientes/Cliente.java
@@ -0,0 +1,35 @@
+package com.github.youssfbr.clientes.clientes;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Cliente(String nome , Long id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
